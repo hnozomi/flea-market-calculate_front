2470f06665d391a0f8375742c84c6dda194a0ae7
nozomi <47496184+hnozomi@users.noreply.github.com>
2022-10-09 23:44:56 +0900

    利益から販売価格を求める計算

--- src/components/page/main/components/MainContent.tsx
+++ src/components/page/main/components/MainContent.tsx
@@ -16,7 +16,9 @@ import { MainPageProps } from '../hooks/useMainPageHook';
 
 export const MainContent: FC<MainPageProps> = ({
   onClickProfitCalculate,
+  onClickSellingPriceCalculate,
   profit,
+  profit1,
   setSellingPrice,
   setShippingMethod,
 }) => {
@@ -27,6 +29,8 @@ export const MainContent: FC<MainPageProps> = ({
   const onChangeShippingMethod = (e: any) => {
     setShippingMethod(e.target.value);
   };
+
+  console.log(profit1);
 
   return (
     <Box mx="1rem">
@@ -82,6 +86,61 @@ export const MainContent: FC<MainPageProps> = ({
           <StatNumber>{`￥${profit.paypay.profit}`}</StatNumber>
           <StatHelpText>{`手数料￥${profit.paypay.commission}`}</StatHelpText>
           <StatHelpText>{`配送料￥${profit.paypay.fee}`}</StatHelpText>
+        </Stat>
+      </Flex>
+      <Box mt="1rem">
+        <Text>確保したい利益</Text>
+        <Input placeholder="￥" type="tel" onChange={onChangeSellingPrice} />
+      </Box>
+      <Box mt="1rem">
+        <Text>配送方法</Text>
+        <Select placeholder="配送方法を選択" onChange={onChangeShippingMethod}>
+          <option value="nekoposu">ネコポス</option>
+          <option value="yupocket">ゆうパケット</option>
+          <option value="yupacketpost">ゆうパケットポスト</option>
+          <option value="deliverycompact">宅急便コンパクト</option>
+          <option value="delivery60">宅急便(60サイズ)</option>
+          <option value="delivery80">宅急便(80サイズ)</option>
+          <option value="delivery100">宅急便(100サイズ)</option>
+          <option value="delivery120">宅急便(120サイズ)</option>
+          <option value="delivery140">宅急便(140サイズ)</option>
+          <option value="delivery160">宅急便(160サイズ)</option>
+          <option value="delivery180">宅急便(180サイズ)</option>
+          <option value="delivery200">宅急便(200サイズ)</option>
+          <option value="yupack60">ゆうパック(60サイズ)</option>
+          <option value="yupack80">ゆうパック(80サイズ)</option>
+          <option value="yupack100">ゆうパック(100サイズ)</option>
+          <option value="yupack120">ゆうパック(120サイズ)</option>
+          <option value="yupack140">ゆうパック(140サイズ)</option>
+          <option value="yupack160">ゆうパック(160サイズ)</option>
+          <option value="yupack170">ゆうパック(170サイズ)</option>
+          <option value="yupacketplus">ゆうパケットプラス</option>
+        </Select>
+      </Box>
+      <Box textAlign="right">
+        <Button mt="1rem" onClick={onClickSellingPriceCalculate}>
+          計算する
+        </Button>
+      </Box>
+
+      <Flex mt="2rem">
+        <Stat borderWidth={1} p="1rem">
+          <StatLabel>メルカリ</StatLabel>
+          <StatNumber>{`￥${profit1.merukari.sellingPrice}`}</StatNumber>
+          <StatHelpText>{`手数料￥${profit1.merukari.commission}`}</StatHelpText>
+          <StatHelpText>{`配送料￥${profit1.merukari.fee}`}</StatHelpText>
+        </Stat>
+        <Stat borderWidth={1} p="1rem">
+          <StatLabel>ラクマ</StatLabel>
+          <StatNumber>{`￥${profit1.rakuma.sellingPrice}`}</StatNumber>
+          <StatHelpText>{`手数料￥${profit1.rakuma.commission}`}</StatHelpText>
+          <StatHelpText>{`配送料￥${profit1.rakuma.fee}`}</StatHelpText>
+        </Stat>
+        <Stat borderWidth={1} p="1rem">
+          <StatLabel>PayPayフリマ</StatLabel>
+          <StatNumber>{`￥${profit1.paypay.sellingPrice}`}</StatNumber>
+          <StatHelpText>{`手数料￥${profit1.paypay.commission}`}</StatHelpText>
+          <StatHelpText>{`配送料￥${profit1.paypay.fee}`}</StatHelpText>
         </Stat>
       </Flex>
     </Box>
--- src/components/page/main/hooks/useMainPageHook.ts
+++ src/components/page/main/hooks/useMainPageHook.ts
@@ -9,13 +9,19 @@ export const useMainPageHook = () => {
     rakuma: 0.06,
   };
 
-  const [sellingPrice, setSellingPrice] = useState(0);
+  const [sellingPrice, setSellingPrice] = useState<number>(0);
   const [shippingMethod, setShippingMethod] =
     useState<ShippingMethodKeyType>('noSelect');
   const [profit, setProfit] = useState({
     merukari: { commission: 0, fee: 0, profit: 0 },
     paypay: { commission: 0, fee: 0, profit: 0 },
     rakuma: { commission: 0, fee: 0, profit: 0 },
+  });
+
+  const [profit1, setProfit1] = useState({
+    merukari: { commission: 0, fee: 0, sellingPrice: 0 },
+    paypay: { commission: 0, fee: 0, sellingPrice: 0 },
+    rakuma: { commission: 0, fee: 0, sellingPrice: 0 },
   });
 
   const onClickProfitCalculate = () => {
@@ -50,13 +56,55 @@ export const useMainPageHook = () => {
         profit: rakumaProfit,
       },
     });
+  };
 
-    return { merukariCommission, paypayCommission, rakumaCommission };
+  const onClickSellingPriceCalculate = () => {
+    const a = Number(sellingPrice) + DELIVERY_FEE[shippingMethod]['rakuma'];
+    const b = 10;
+    const c = 9.34;
+    const rakumaSellingPrice = Math.floor((a * b) / c);
+
+    const d = Number(sellingPrice) + DELIVERY_FEE[shippingMethod]['merukari'];
+    const e = 10;
+    const f = 9;
+
+    const merukariSellingPrice = Math.floor((d * e) / f);
+
+    const g = Number(sellingPrice) + DELIVERY_FEE[shippingMethod]['paypay'];
+    const h = 10;
+    const i = 9.5;
+
+    const paypaySellingPrice = Math.floor((g * h) / i);
+
+    const rakumaDeliveryFee = DELIVERY_FEE[shippingMethod]['rakuma'];
+    const merukariDeliveryFee = DELIVERY_FEE[shippingMethod]['merukari'];
+    const paypayDeliveryFee = DELIVERY_FEE[shippingMethod]['paypay'];
+
+    setProfit1({
+      ...profit1,
+      merukari: {
+        commission: 0,
+        fee: merukariDeliveryFee,
+        sellingPrice: merukariSellingPrice,
+      },
+      paypay: {
+        commission: 0,
+        fee: paypayDeliveryFee,
+        sellingPrice: paypaySellingPrice,
+      },
+      rakuma: {
+        commission: 0,
+        fee: rakumaDeliveryFee,
+        sellingPrice: rakumaSellingPrice,
+      },
+    });
   };
 
   return {
     onClickProfitCalculate,
+    onClickSellingPriceCalculate,
     profit,
+    profit1,
     setSellingPrice,
     setShippingMethod,
   };
